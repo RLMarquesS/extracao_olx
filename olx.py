f9b463b060cc60f4ae10aa85c2258472cef97282
rafaelm <rafaelmarques.contato01@gmail.com>
2025-06-09 21:13:06 -0300

    O script pergunta qual o nome do arquivo o usuário deseja salvar e mostra isso no ternial no final da execução

--- olx.py
+++ olx.py
@@ -12,6 +12,7 @@ options.headless = False #Eu preciso ver a janela aberta até agora
 produto = input('Qual produto você quer pesquiar ?')
 
 
+
 #Acessando o site 
 driver = webdriver.Firefox(options=options)
 driver.get(f"https://www.olx.com.br/estado-ce?q={produto}")
@@ -23,10 +24,14 @@ produtos = driver.find_elements(By.XPATH,"//h2[@class='olx-text olx-text--body-l
 precos = driver.find_elements(By.XPATH,"//h3[@class='olx-text olx-text--body-large olx-text--block olx-text--semibold olx-adcard__price']")
 links = driver.find_elements(By.XPATH,"//a[@class='olx-adcard__link']")
 
+
+nome_arquivo = input('Deseja salvar esse arquivo com qual nome? ')
+
+
 for produto,preco,link in zip(produtos,precos,links):
-    with open('resultado.csv','a',encoding='utf-8') as arquivo:
+    with open(f'{nome_arquivo}.csv','a',encoding='utf-8') as arquivo:
         arquivo.write(f'{produto.text},{preco.text},{link.get_attribute("href")}{os.linesep}')
 
 
-print('Sua cotação foi salva, obrigado!')
+print(f'Sua cotação foi salva no arquivo {nome_arquivo}.csv, obrigado!')
 driver.quit()
